a0379842204e5ebfa95dc75b3587dc437fe99313
Harrison Hocker <hjhocker@gmail.com>
2015-07-28 21:42:51 -0500

    Updated controller to present database information.

--- src/main/java/org/test/demo/controllers/HelloController.java
+++ src/main/java/org/test/demo/controllers/HelloController.java
@@ -1,14 +1,29 @@
 package org.test.demo.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.test.demo.domain.Account;
+import org.test.demo.repository.AccountRepository;
 
 @RestController
 @RequestMapping(value = "/api")
 public class HelloController {
 
+	@Autowired
+	private AccountRepository accountRepository;
+	
 	@RequestMapping(value = "/hello")
 	public String hello() {
 		return "Hello World\n";
+	}
+	
+	@RequestMapping(value = "/getName")
+	public ResponseEntity<List<Account>> getAccountName() {
+		return new ResponseEntity<>(accountRepository.findByAccountId(1L), HttpStatus.OK);
 	}
 }
