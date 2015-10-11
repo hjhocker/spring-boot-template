c514f5fba4f81cc48a649963c128b187e4b54d00
Harrison Hocker <hjhocker@gmail.com>
2015-10-11 10:05:41 -0500

    Iron ajax WIP

--- client/setup_polymer.sh
+++ client/setup_polymer.sh
@@ -2,5 +2,7 @@
 
 bower init 
 bower install --save Polymer/polymer#^1.1.0
-bower install polymer-currency-filter
+bower install --save Polymer/core-ajax#^0.5
+bower install --save polymer-currency-filter
+bower install --save polymer-element/iron-ajax
 bower update
