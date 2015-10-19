88790b0b262a9c6618c889aa12228d54fdf6ddbb
Harrison Hocker <hjhocker@gmail.com>
2015-10-19 00:35:29 -0500

    More routing pages

--- client/js/main.js
+++ client/js/main.js
@@ -1,7 +1,19 @@
 var app = angular.module('bankApp', ['ngRoute']);
 
 app.config(function($routeProvider) {
-  $routeProvider.when('/', {
-    templateUrl: 'partials/home.html'
-  })
+  $routeProvider
+    .when('/', {
+      templateUrl: 'partials/home.html'
+      controller: 'HomeController'
+    })
+    .when('/accounts', {
+      templateUrl: 'partials/accounts.html'
+    })
+    .otherwise({
+      redirectTo: 'partials/error.html'
+    })
+});
+
+app.controller('HomeController', function($scope) {
+
 });
